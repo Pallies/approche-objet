5feaa024c1a4514ebb623878959f1d72f13fdfa4
Palliès <palliescie@gmail.com>
2021-10-08 10:54:17 +0200

    tp 03

--- src/fr/diginamic/operations/Operations.java
+++ src/fr/diginamic/operations/Operations.java
@@ -2,4 +2,7 @@ package fr.diginamic.operations;
 
 public class Operations {
 
+	public double calcul(double chiffre1,double chiffre2,char operateur) {
+		return chiffre1 +operateur +chiffre2;
+	}
 }
