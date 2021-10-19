bb4ec1e8991ab4f146e15a9ee5ed63642a7a9451
Palliès <palliescie@gmail.com>
2021-10-19 13:40:08 +0200

    produit attribut

---
+++ src/fr/nutriscores/openfoodfacts/models/Produit.java
@@ -0,0 +1,16 @@
+package fr.nutriscores.openfoodfacts.models;
+
+public class Produit {
+
+	private Categorie categorie;
+	private Marque marque;
+	private String nom;
+	private String nutritionGradeFr;
+	private List<Element> elements;
+	private List<Ingredient> ingredients;
+	private List<Allergene> allergenes;
+	private List<Additif> additifs;
+
+
+
+}
