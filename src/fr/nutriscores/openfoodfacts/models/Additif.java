f65de0762bd929633b1896bc14f5cdda4cc57c40
Palliès <palliescie@gmail.com>
2021-10-24 22:09:02 +0200

    repo traitement fichier

--- src/fr/nutriscores/openfoodfacts/models/Additif.java
+++
@@ -1,9 +0,0 @@
-package fr.nutriscores.openfoodfacts.models;
-
-public class Additif extends Descriptif{
-
-	public Additif(String nom) {
-		super(nom);
-	}
-
-}
--- src/fr/nutriscores/openfoodfacts/models/Allergene.java
+++
@@ -1,9 +0,0 @@
-package fr.nutriscores.openfoodfacts.models;
-
-public class Allergene extends Descriptif{
-
-	public Allergene(String nom) {
-		super(nom);
-	}
-
-}
